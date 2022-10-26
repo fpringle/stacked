5e582b587d1f87223514d49b17139f9fd79c6b0e
fpringle <freddyjepringle@gmail.com>
2022-10-26 21:41:51 +0200

    make sure the src/build directory exists

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p src/build
+
 echo '(function() {' > src/build/stacked.js
 
 cat src/language.js src/editor.js src/map.js src/player.js src/levels.js src/game.js src/index.js >> src/build/stacked.js
